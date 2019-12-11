cff8c8e4583efa62d74ca1bf40881445eb64aec2
Nate <nathan.mann@me.com>
2019-12-10 21:35:31 -0500

    dfsfg

--- Package.swift
+++ Package.swift
@@ -13,10 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         dependencies: [
-             .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
-             .package(url: "https://github.com/ReactiveX/RxCocoa.git", .upToNextMajor(from: "5.0.0")),
-         ]
+         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+         .package(url: "https://github.com/ReactiveX/RxCocoa.git", .upToNextMajor(from: "5.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
